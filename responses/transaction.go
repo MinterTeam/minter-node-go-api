3908c4a48f258c922a40da1442003f307bf6a79a
Daniil Dulin <daniildulin@gmail.com>
2019-06-11 13:24:25 +0300

    get transactions by query

--- responses/transaction.go
+++ responses/transaction.go
@@ -9,6 +9,13 @@ type TransactionResponse struct {
 	Result  Transaction `json:"result"`
 }
 
+type TransactionsResponse struct {
+	Jsonrpc string        `json:"jsonrpc"`
+	ID      string        `json:"id"`
+	Error   *ErrorData    `json:"error"`
+	Result  []Transaction `json:"result"`
+}
+
 type Transaction struct {
 	Hash        string             `json:"hash"`
 	From        string             `json:"from"`
